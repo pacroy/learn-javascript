bad3b8ef7c455791e4fe4ec2e98076a42950061a
Par <pacroy@users.noreply.github.com>
2022-07-09 19:53:16 +0700

    Add dynamically-typed

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ console.log(interestRate)
 let isApproved = false
 let undefinedVariable = undefined   // Variable is undefined when declared
 let nullVariable = null // We use null to clear the value
+
+// JavaScript is a dynamic (dynamically-typed) language
+console.log(typeof(nickName))   // string
+nickName = 1
+console.log(nickName)
+console.log(typeof(nickName))   // number
+nickName = 30.1
+console.log(nickName)
+console.log(typeof(nickName))   // number
+nickName = undefined
+console.log(nickName)
+console.log(typeof(nickName))   // undefined
+nickName = null
+console.log(nickName)
+console.log(typeof(nickName))   // object
