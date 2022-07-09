bcf9bd489df5818d42542407dc7ddfb4c689d840
Par <pacroy@users.noreply.github.com>
2022-07-09 19:48:49 +0700

    Add boolean, undefined, null

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ let firstName = 'Maxine', lastName = 'Mayfield'
 const interestRate = 0.3
 // interestRate = 1 // This will end up in TypeError
 console.log(interestRate)
+
+let isApproved = false
+let undefinedVariable = undefined   // Variable is undefined when declared
+let nullVariable = null // We use null to clear the value
